0428d8cc9e3df3ad95d70d7448f9a64d800aea78
Ganesh Guntuku <s170220@rguktsklm.ac.in>
2023-12-20 14:17:41 +0530

    Update: Logout updated.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,13 @@ import IssuedBooks from "./Pages/IssuedBooks/IssuedBooks";
 import SubmitBookRequest from "./Pages/SubmitBookRequest/SubmitBookRequest";
 import PendingFine from "./Pages/PendingFine/PendingFine";
 import BookStatus from "./Pages/BookStatus/BookStatus";
+import { Provider } from "react-redux";
+import store from "./API/store";
+import Logout from "./Pages/Logout";
 
 const App = () => {
   return (
+    <Provider store={store}>
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
@@ -17,8 +21,10 @@ const App = () => {
         <Route path="/bookstatus" element={<BookStatus />} />
         <Route path="/issuedbooks" element={<IssuedBooks />} />
         <Route path="/pendingfine" element={<PendingFine />} />
+        <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </Router>
+    </Provider>
   );
 };
 
